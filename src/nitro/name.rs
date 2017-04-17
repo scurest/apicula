623a7d02f99837a548d355f806e5b67decc7d17c
scurest <scurest@users.noreply.github.com>
2017-04-16 20:27:55 -0500

    (ref) Slight notational improvements for mod name

--- src/nitro/name.rs
+++ src/nitro/name.rs
@@ -2,14 +2,18 @@ use std::fmt;
 use std::fmt::Write;
 use util::view::Viewable;
 
+/// Sixteen-byte NUL-padded ASCII(?) string, used as human-readable names
+/// in Nitro files.
 #[derive(Copy, Clone, PartialEq, Eq, Hash)]
 pub struct Name(pub [u8; 16]);
 
 impl Name {
     pub fn from_bytes(buf: &[u8]) -> Name {
+        assert_eq!(buf.len(), 16);
+
         let mut arr = [0; 16];
-        for (i, &b) in buf.iter().enumerate() {
-            arr[i] = b;
+        for i in 0..16 {
+            arr[i] = buf[i];
         }
         Name(arr)
     }
@@ -24,7 +28,9 @@ impl Viewable for Name {
 
 impl fmt::Display for Name {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for &b in trim_trailing_nul(&self.0[..]) {
+        for &b in trim_trailing_nuls(&self.0[..]) {
+            // Convert non-printable characters to periods (which is what
+            // hex editors usually do).
             f.write_char(if b < 0x20 { '.' } else { b as char })?;
         }
         Ok(())
@@ -34,7 +40,7 @@ impl fmt::Display for Name {
 impl fmt::Debug for Name {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         f.write_char('"')?;
-        for &b in trim_trailing_nul(&self.0[..]) {
+        for &b in trim_trailing_nuls(&self.0[..]) {
             for c in (b as char).escape_default() {
                 f.write_char(c)?;
             }
@@ -49,16 +55,16 @@ pub struct IdFmt<'a>(pub &'a Name);
 
 impl<'a> fmt::Display for IdFmt<'a> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let trimmed = trim_trailing_nul(&(self.0).0[..]);
+        let trimmed = trim_trailing_nuls(&(self.0).0[..]);
         if trimmed.len() == 0 {
             f.write_char('_')?;
             return Ok(());
         }
         for &b in trimmed {
             let is_letter_or_digit =
-                (b >= 'a' as u8 && b <= 'z' as u8) ||
-                (b >= 'A' as u8 && b <= 'Z' as u8) ||
-                (b >= '0' as u8 && b <= '9' as u8);
+                (b >= b'a' && b <= b'z') ||
+                (b >= b'A' && b <= b'Z') ||
+                (b >= b'0' && b <= b'9');
             let c = if is_letter_or_digit { b as char } else { '_' };
             f.write_char(c)?;
         }
@@ -66,7 +72,7 @@ impl<'a> fmt::Display for IdFmt<'a> {
     }
 }
 
-fn trim_trailing_nul(mut buf: &[u8]) -> &[u8] {
+fn trim_trailing_nuls(mut buf: &[u8]) -> &[u8] {
     while let Some((&0, rest)) = buf.split_last() {
         buf = rest;
     }
