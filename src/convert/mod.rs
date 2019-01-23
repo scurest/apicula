1797bf0c396bd71576cc03f0c1dd376289836dd0
scurest <scurest@users.noreply.github.com>
2019-01-22 20:11:26 -0600

    Create the output directory lazily

--- src/convert/mod.rs
+++ src/convert/mod.rs
@@ -14,11 +14,20 @@ use connection::{Connection, ConnectionOptions};
 
 pub fn main(matches: &ArgMatches) -> Result<()> {
     let out_dir = PathBuf::from(matches.value_of("OUTPUT").unwrap());
-    fs::create_dir(&out_dir)
-        .chain_err(||
-            "output directory could not be created -- maybe it \
-            already exists?"
-        )?;
+    // We'll create the output dir lazily when we first write a file to it,
+    let mut out_dir_created = false;
+    let mut ensure_out_dir_created = || -> Result<()> {
+        if out_dir_created {
+            return Ok(());
+        }
+        fs::create_dir(&out_dir)
+            .chain_err(||
+                "output directory could not be created -- maybe it \
+                already exists?"
+            )?;
+        out_dir_created = true;
+        Ok(())
+    };
 
     let db = Database::from_arg_matches(matches)?;
 
@@ -42,6 +51,8 @@ pub fn main(matches: &ArgMatches) -> Result<()> {
 
     // Save each model as a COLLADA file
     for (model_id, model) in db.models.iter().enumerate() {
+        ensure_out_dir_created()?;
+
         let name = model_file_namer.get_fresh_name(format!("{}", model.name.print_safe()));
         let model_file_path = out_dir.join(&format!("{}.{}", name, format));
         let mut f = File::create(model_file_path)?;
@@ -86,6 +97,8 @@ pub fn main(matches: &ArgMatches) -> Result<()> {
                 continue;
             }
         };
+
+        ensure_out_dir_created()?;
 
         let path = out_dir.join(&format!("{}.png", image_name));
         match write_rgba(&path, &rgba.0[..], texture.params.width(), texture.params.height()) {
