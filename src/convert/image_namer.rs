f2cb379a372c2e363fbb1ae7f8c2ba721bee4a9e
scurest <scurest@users.noreply.github.com>
2019-01-22 20:29:17 -0600

    Use patterns for image discovery

--- src/convert/image_namer.rs
+++ src/convert/image_namer.rs
@@ -4,9 +4,11 @@ use std::collections::HashMap;
 use util::namers::UniqueNamer;
 use connection::Connection;
 
+type ImageId = (TextureId, Option<PaletteId>);
+
 pub struct ImageNamer {
     pub namer: UniqueNamer,
-    pub names: HashMap<(TextureId, Option<PaletteId>), String>,
+    pub names: HashMap<ImageId, String>,
 }
 
 impl ImageNamer {
@@ -16,22 +18,49 @@ impl ImageNamer {
             names: HashMap::new(),
         };
 
+        // Discovery images from model materials
         for mdl_conn in &conn.models {
             for mat_conn in &mdl_conn.materials {
                 match mat_conn.image_id() {
-                    Ok(Some(image_id)) => {
-                        let texture_name = db.textures[image_id.0].name;
-                        let namer = &mut image_namer.namer;
-                        image_namer.names.entry(image_id).or_insert_with(|| {
-                            namer.get_fresh_name(texture_name.print_safe().to_string())
-                        });
-                    }
+                    Ok(Some(image_id)) =>
+                        image_namer.insert_image_id(db, image_id),
                     _ => continue,
                 }
             }
         }
 
+        // Discover images from pattern animations
+        for mdl_conn in &conn.models {
+            for pat_conn in &mdl_conn.patterns {
+                let pat = &db.patterns[pat_conn.pattern_id];
+                for track in &pat.material_tracks {
+                    for keyframe in &track.keyframes {
+                        let tex_idx = keyframe.texture_idx as usize;
+                        let texture_id = match pat_conn.texture_ids[tex_idx] {
+                            Some(id) => id,
+                            None => continue,
+                        };
+                        let pal_idx = keyframe.palette_idx as usize;
+                        let palette_id = match pat_conn.palette_ids[pal_idx] {
+                            Some(id) => id,
+                            None => continue,
+                        };
+                        let image_id = (texture_id, Some(palette_id));
+                        image_namer.insert_image_id(db, image_id);
+                    }
+                }
+            }
+        }
+
         image_namer
+    }
+
+    pub fn insert_image_id(&mut self, db: &Database, image_id: ImageId) {
+        let texture_name = db.textures[image_id.0].name;
+        let namer = &mut self.namer;
+        self.names.entry(image_id).or_insert_with(|| {
+            namer.get_fresh_name(texture_name.print_safe().to_string())
+        });
     }
 
     pub fn add_more_images(&mut self, db: &Database) {
